3228c60825e53316794787700760d5bb24164ecc
Matt <langma@gmail.com>
2019-07-04 23:19:42 -0500

    bug fixes

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 mkdir .install_tmp
-cd .install_tmp
 
 apt-get update
 
@@ -20,16 +19,18 @@ apt-get install \
   python3 \
   vagrant 
 
+update-alternatives --install /usr/bin/python python /usr/bin/python3 1
+
 # protoc
 apt-get -y install autoconf automake libtool make g++ unzip
-curl -f sSLo ./protoc.zip https://github.com/protocolbuffers/protobuf/releases/download/v3.8.0/protoc-3.8.0-linux-x86_64.zip
-unzip protoc.zip -x readme.txt -d /usr/local/
+curl -sSLo .install_tmp/protoc.zip https://github.com/protocolbuffers/protobuf/releases/download/v3.8.0/protoc-3.8.0-linux-x86_64.zip
+unzip .install_tmp/protoc.zip -x readme.txt -d /usr/local/
 
 # golang
-curl https://dl.google.com/go/go1.12.6.linux-amd64.tar.gz
-tar -C /usr/local -xzf go1.12.6.linux-amd64.tar.gz
-echo 'export GOPATH="$HOME/go"' >> /etc/profile
-echo 'PATH="$PATH:/usr/local/go/bin:$GOPATH/bin:$HOME/.local/bin"' >> /etc/profile
+curl -sSLo .install_tmp/go.tgz https://dl.google.com/go/go1.12.6.linux-amd64.tar.gz
+tar -C /usr/local -xzf .install_tmp/go.tgz
+sudo -u $SUDO_USER echo 'export GOPATH="$HOME/go"' >> $HOME/.bashrc
+sudo -u $SUDO_USER echo 'PATH="$PATH:/usr/local/go/bin:$GOPATH/bin:$HOME/.local/bin"' >> $HOME/.bashrc
 export GOPATH=$HOME/go
 export PATH=$PATH:/usr/local/go/bin:$GOPATH/bin:$HOME/.local/bin
 go get google.golang.org/grpc
@@ -44,29 +45,25 @@ apt-get -y install \
   curl \
   gnupg-agent \
   software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository \
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository \
   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) \
   stable"
 apt-get update
-apt-get install docker-ce docker-ce-cli contianerd.io
+apt-get install docker-ce docker-ce-cli containerd.io
 
 # gcloud
-echo "deb https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
-curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add - || curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-apt-get update && apt get install google-cloud-sdk
+echo "deb https://packages.cloud.google.com/apt cloud-sdk main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+apt-get update && apt-get install google-cloud-sdk
 
 # vim
 update-alternatives --set editor /usr/bin/vim
-sudo -u $USER cp vimrc $HOME/.vimrc
-git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
-sudo -u $USER mkdir $HOME/.vimswp
+sudo -u $SUDO_USER cp vimrc $HOME/.vimrc
+sudo -u $SUDO_USER git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
+sudo -u $SUDO_USER mkdir $HOME/.vimswp
+sudo -u $SUDO_USER vim +PluginInstall +qall || true
+sudo -u $SUDO_USER $HOME/.vim/bundle/YouCompleteMe/install.py || true
 
-# TODO 
-
-vim +PluginInstall +qall || true
-$HOME/.vim/bundle/YouCompleteMe/install.py || true
-
-cd ..
 rm -rf .install_tmp
