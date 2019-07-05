b84d68c4647bbb643140dae940ef0a33412d216c
Matt <langma@gmail.com>
2019-07-05 00:41:04 -0500

    added optional goland install, skip protoc/go if they are already installed.

--- install.sh
+++ install.sh
@@ -22,15 +22,19 @@ apt-get -y install \
 update-alternatives --install /usr/bin/python python /usr/bin/python3 1
 
 # protoc
-apt-get -y install autoconf automake libtool make g++ unzip
-curl -sSLo .install_tmp/protoc.zip https://github.com/protocolbuffers/protobuf/releases/download/v3.8.0/protoc-3.8.0-linux-x86_64.zip
-unzip .install_tmp/protoc.zip -x readme.txt -d /usr/local/
+if [[ ! -d "/usr/local/protoc" ]]; then
+  apt-get -y install autoconf automake libtool make g++ unzip
+  curl -sSLo .install_tmp/protoc.zip https://github.com/protocolbuffers/protobuf/releases/download/v3.8.0/protoc-3.8.0-linux-x86_64.zip
+  unzip .install_tmp/protoc.zip -x readme.txt -d /usr/local/
+fi
 
 # golang
-curl -sSLo .install_tmp/go.tgz https://dl.google.com/go/go1.12.6.linux-amd64.tar.gz
-tar -C /usr/local -xzf .install_tmp/go.tgz
-sudo -u $SUDO_USER echo 'export GOPATH="$HOME/go"' >> $HOME/.bashrc
-sudo -u $SUDO_USER echo 'PATH="$PATH:/usr/local/go/bin:$GOPATH/bin:$HOME/.local/bin"' >> $HOME/.bashrc
+if [[ ! -d "/usr/local/go" ]]; then
+  curl -sSLo .install_tmp/go.tgz https://dl.google.com/go/go1.12.6.linux-amd64.tar.gz
+  tar -C /usr/local -xzf .install_tmp/go.tgz
+  sudo -u $SUDO_USER echo 'export GOPATH="$HOME/go"' >> $HOME/.bashrc
+  sudo -u $SUDO_USER echo 'PATH="$PATH:/usr/local/go/bin:$GOPATH/bin:$HOME/.local/bin"' >> $HOME/.bashrc
+fi
 export GOPATH=$HOME/go
 export PATH=$PATH:/usr/local/go/bin:$GOPATH/bin:$HOME/.local/bin
 go get google.golang.org/grpc
@@ -65,5 +69,11 @@ sudo -u $SUDO_USER git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.
 sudo -u $SUDO_USER mkdir $HOME/.vimswp
 sudo -u $SUDO_USER vim +PluginInstall +qall || true
 sudo -u $SUDO_USER $HOME/.vim/bundle/YouCompleteMe/install.py || true
+
+read -p "Install GoLand? [y|n] " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+  ./goland_install.sh
+fi
 
 rm -rf .install_tmp
