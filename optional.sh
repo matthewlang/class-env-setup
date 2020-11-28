bcd2fee5e99b61bb18d9850f4f23fc59083aba6c
matthewlang <langm@rhodes.edu>
2020-11-28 16:33:41 -0600

    update goland, clion, atom versions

--- optional.sh
+++ optional.sh
@@ -7,15 +7,15 @@ mkdir .install_tmp
 read -p "Install GoLand? [y|n] " -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-  if [ -d "/opt/GoLand-2019.1.3" ]; then
+  if [ -d "/opt/GoLand-2020.2.3" ]; then
     echo "GoLand already installed..."
   else
     if [ ! -f "goland.tgz" ]; then
-      curl -sSLo goland.tgz https://download.jetbrains.com/go/goland-2019.1.3.tar.gz
+      curl -sSLo goland.tgz https://download.jetbrains.com/go/goland-2020.2.3.tar.gz
     fi
     tar xzf goland.tgz -C /opt/
     rm goland.tgz
-    sudo -u $SUDO_USER echo "alias goland='/opt/GoLand-2019.1.3/bin/goland.sh'" >> $HOME/.bashrc
+    sudo -u $SUDO_USER echo "alias goland='/opt/GoLand-2020.2.3/bin/goland.sh'" >> $HOME/.bashrc
     echo "To run GoLand, run goland from the terminal."
   fi
 fi
@@ -23,15 +23,15 @@ fi
 read -p "Install CLion? [y|n] " -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-  if [ -d "/opt/CLion-2019.3.2" ]; then
+  if [ -d "/opt/CLion-2020.2.5" ]; then
     echo "CLion already installed..."
   else
     if [ ! -f "clion.tgz" ]; then
-      curl -sSLo clion.tgz https://download.jetbrains.com/cpp/CLion-2019.3.2.tar.gz
+      curl -sSLo clion.tgz https://download.jetbrains.com/cpp/CLion-2020.2.5.tar.gz
     fi
     tar xzf clion.tgz -C /opt/
     rm clion.tgz
-    sudo -u $SUDO_USER echo "alias clion='/opt/clion-2019.3.2/bin/clion.sh'" >> $HOME/.bashrc
+    sudo -u $SUDO_USER echo "alias clion='/opt/clion-2020.2.5/bin/clion.sh'" >> $HOME/.bashrc
     echo "For Rust support, open CLion and install the Rust plugin."
     echo "To run CLion, run clion from the terminal."
   fi
@@ -49,7 +49,7 @@ echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
   apt-get -y install gconf2 gconf-service
   apt --fix-broken install
-  curl -sSLo .install_tmp/atom.deb https://github.com/atom/atom/releases/download/v1.38.2/atom-amd64.deb
+  curl -sSLo .install_tmp/atom.deb https://github.com/atom/atom/releases/download/v1.53.0/atom-amd64.deb
   dpkg -i .install_tmp/atom.deb
   echo "For Go support, open atom and install the go-plus package."
   echo "For Rust support, open atom and install the ide-rust package."
